9762b198a07f53529c21a45840b2eed82fcb2352
Bumios <nhatduy270996@gmail.com>
2019-05-10 11:44:09 +0700

    Init file and add some useful extension.

---
+++ Custom_Calendar/View/Cell/CalendarCollectionCell.swift
@@ -0,0 +1,12 @@
+//
+//  CalendarCollectionCell.swift
+//  Custom_Calendar
+//
+//  Created by Duy Tran N. on 5/10/19.
+//  Copyright © 2019 MBA0204. All rights reserved.
+//
+
+import UIKit
+
+final class CalendarCollectionCell: UICollectionViewCell {
+}
